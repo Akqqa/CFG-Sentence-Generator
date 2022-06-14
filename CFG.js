437385a84271e180a3c8e903240206833f09c506
Alex Krajnovic <93438896+Akqqa@users.noreply.github.com>
2022-06-14 15:26:19 +0100

    Added protection against infinite grammars and excessive sentences

--- CFG.js
+++ CFG.js
@@ -21,23 +21,44 @@ function buildGrammar(cfginput) {
 
 // Generate random sentence
 function generateSentence(productions) {
-	var sentence = "SENTENCE";
   let allproductions = Object.keys(productions);
   let prodregex = new RegExp(allproductions.join("|") + "|EPSILON");
+  let generating = true;
+  let baseDate = new Date();
   
-  let finished = false;
-  while (!finished) {
-  	let match = sentence.match(prodregex);
-    if (match == null) {
-    	finished = true;
-      break;
-    } else if (match == "EPSILON") {
-    	sentence = sentence.replace("EPSILON", "");
-    } else {
-    	var replacement = productions[match][Math.floor(Math.random()*productions[match].length)];
-      sentence = sentence.replace(match, replacement);
+  while (generating) {
+  	var sentence = "SENTENCE";
+  
+    let finished = false;
+    const startDate = new Date();
+    
+    console.log(baseDate.getTime() - startDate.getTime());
+    if (startDate.getTime() - baseDate.getTime() > 1000) {
+    	return "Error: Valid sentence taken too long to generate. Please check the CFG to ensure there are no infinite loops"
+    }
+
+    while (!finished) {
+    	// Discards if taking too long to generate
+      let endDate = new Date();
+    	if (endDate.getTime() - startDate.getTime() > 50) {
+        console.log("abort");
+      	break;
+      }
+    
+      let match = sentence.match(prodregex);
+      if (match == null) {
+        finished = true;
+        generating = false;
+        break;
+      } else if (match == "EPSILON") {
+        sentence = sentence.replace("EPSILON", "");
+      } else {
+        var replacement = productions[match][Math.floor(Math.random()*productions[match].length)];
+        sentence = sentence.replace(match, replacement);
+      }
     }
   }
+	
   return sentence;
 }
 
